994bd8b585bae15fb34691e06f3a86e30da8bac7
Spencer Malott <spencermalott16@gmail.comgit config --global user.email spencermalott16@gmail.com>
2021-11-09 00:15:29 +0000

    completed exercise 5-7 - see readme for exercise details

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,15 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+Store.has_many :employees
+Employee.belongs_to :store
+
+@store1.employees.create(first_name: "Kurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Andy", last_name: "Dwyer", hourly_rate: 10)
+@store1.employees.create(first_name: "Sarah", last_name: "Michaels", hourly_rate: 45)
+@store1.employees.create(first_name: "Shonda", last_name: "Rimes", hourly_rate: 120)
+
+@store2.employees.create(first_name: "Clare", last_name: "Woods", hourly_rate: 37)
+@store2.employees.create(first_name: "Kevin", last_name: "Smith", hourly_rate: 25)
+@store2.employees.create(first_name: "Ahmed", last_name: "Hassan", hourly_rate: 75)
+@store2.employees.create(first_name: "Chelsea", last_name: "Woods", hourly_rate: 50)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,24 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+Employee.validates :first_name, :last_name, :store_id, presence: true
+Employee.validates :hourly_rate, numericality: { only_integer: true }
+Store.validates :name, length: { minimum: 3}
+Store.validates :annual_revenue, numericality: { greater_than_or_equal_to: 0}
+
+puts "Please input a store name:"
+print ">"
+store_name = $stdin.gets.chomp
+
+@new_store = Store.new(name: store_name)
+puts @new_store.valid?
+puts @new_store.errors.full_messages
+
+@new_store = Store.new(name: store_name, annual_revenue: 100000)
+puts @new_store.valid?
+puts @new_store.errors.full_messages
+
+@employee = Employee.new()
+puts @employee.valid?
+puts @employee.errors.full_messages
+
